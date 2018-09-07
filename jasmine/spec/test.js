f206858cb212ac88c22c685e7806aee074e07257
Krista Richmond <krista.trista@me.com>
2018-09-07 12:18:52 -0400

    first menu test complete

---
+++ jasmine/spec/test.js
@@ -0,0 +1,11 @@
+//loop through all feeds and make sure url is defined and is not empty
+// var urls;
+// for(i = 0; i < allFeeds.length - 1; i++){
+//    urls = allFeeds[i].url;
+// };
+//
+//loop through all feeds and make sure name is defines and is not empty
+// var feedName;
+// for(i = 0; i < allFeeds.length - 1; i++){
+//    feedName = allFeeds[i].name;
+// };
