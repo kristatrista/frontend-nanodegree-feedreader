f3cff133fdb13a21c0fa7bdea1cd19ebd9ff0bb5
Krista Richmond <krista.trista@me.com>
2018-09-10 17:03:22 -0400

    new feed test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,7 +61,7 @@ $(function() {
        * hiding/showing of the menu element.
        */
        it('is hidden by default',function(){
-         expect($('body').hasClass('menu-hidden')).toEqual(true);
+      expect($('body').hasClass('menu-hidden')).toEqual(true);
        });
 
        /* TODO: Write a test that ensures the menu changes
@@ -70,27 +70,41 @@ $(function() {
         * clicked and does it hide when clicked again.
         */
       it('toggles visibility when clicked', function(){
-        expect($('body').hasClass('menu-hidden')).toEqual(true);
+        $('.menu-icon-link').trigger('click');
+        expect($('body').hasClass('menu-hidden')).toEqual(false);         $('.menu-icon-link').trigger('click');
+         expect($('body').hasClass('menu-hidden')).toEqual(true);
         });
     });
 
 
-
-
-
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+       beforeEach( function(done){
+         loadFeed(0, function(){
+           done();
+         });
+       });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+       it('has single entry within feed', function(){
+         expect($('.entry .feed')).toBeDefined();
+       });
+
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection',function() {
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+
 }());
